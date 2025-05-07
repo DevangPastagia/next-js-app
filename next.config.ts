d0fa3fee1b0984d2d07f60d31b08da1d214bfb1c
DevangPastagia <devang.pastagia@girirajdigital.com>
2025-05-07 12:31:23 +0530

    feat: configure server for ip in next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["192.168.0.145"], // replace with actual origin
 };
 
 export default nextConfig;
