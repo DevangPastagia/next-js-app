ee90373577b6cd3b7ef6f18ca64ace27bc586c46
DevangPastagia <devang.pastagia@girirajdigital.com>
2025-05-07 12:52:30 +0530

    feat: link component navigation

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,10 +1,26 @@
+import Link from "next/link";
 export default function products() {
+  const productId = 100;
   return (
     <>
+      <Link href="/">Home</Link>
+      <br />
       <h1> Products List </h1>
-      <p> Product 1 </p>
-      <p> Product 2 </p>
-      <p> Product 3 </p>
+      <h2>
+        <Link href="products/1">Product 1</Link>
+      </h2>
+      <h2>
+        <Link href="products/2">Product 2</Link>
+      </h2>
+      <h2>
+        {/* replace: This will replace the current history entry and directly redirect to home page */}
+        <Link href="products/3" replace>
+          Product 3
+        </Link>
+      </h2>
+      <h2>
+        <Link href={`products/${productId}`}>Product {productId}</Link>
+      </h2>
     </>
   );
 }
